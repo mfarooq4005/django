da1b50d2fff410a8d8b2f14c6d7157de308a1170
JustMe <115028308+mfarooq4005@users.noreply.github.com>
2023-09-01 02:51:08 -0400

    Add files via upload

--- services/models.py
+++ services/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 class titles(models.Model):
-    main_title=models.CharField(max_length=16)
+    main_title=models.CharField(max_length=20)
     main_description=models.CharField(max_length=200)
+    image_url = models.CharField(default='default_image_url',max_length=1200)
 # Create your models here.
